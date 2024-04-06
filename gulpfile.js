300beae496ff3581c7637e34f75a7b8a93193dbe
lidongpeng <2683849542@qq.com>
2024-04-06 17:11:33 +0800

    feat: 架构搭建

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const ts = require('gulp-typescript');
+const del = require('del');
+//清除上次构建的产物
+gulp.task('clean', async function () {
+  await del('lib/**');
+  await del('es/**');
+  await del('dist/**');
+});
+// 生成cjs 模块，使用es构建后的产物去打包
+gulp.task('cjs', function () {
+  return gulp
+    .src(['./es/**/*.js'])
+    .pipe(
+      babel({
+        configFile: '../../.babelrc',
+      }),
+    )
+    .pipe(gulp.dest('lib/'));
+});
+// 引用tsconfig，将ts转换成babel
+gulp.task('es', function () {
+  const tsProject = ts.createProject('tsconfig.pro.json', {
+    module: 'ESNext',
+  });
+  // tsc 读取配置 进行babel编译react，将结果输入到 创建的es/目录
+  console.log(tsProject);
+  return tsProject.src().pipe(tsProject()).pipe(babel()).pipe(gulp.dest('es/'));
+});
+//产出声明文件
+gulp.task('declaration', function () {
+  const tsProject = ts.createProject('tsconfig.pro.json', {
+    declaration: true, //是否产出声明文件
+    emitDeclarationOnly: true, //是否只产出声明文件
+  });
+  return tsProject.src().pipe(tsProject()).pipe(gulp.dest('es/')).pipe(gulp.dest('lib/'));
+});
+
+gulp.task('copyReadme', async function () {
+  await gulp.src('../../README.md').pipe(gulp.dest('../../packages/hooks'));
+});
+
+exports.default = gulp.series('clean', 'es', 'cjs', 'declaration', 'copyReadme');
